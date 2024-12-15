5297dc0de4c303a45d168839b7278b9b3ea34306
𝕂𝕪𝕝𝕚𝕒𝕟 <164187100+Kgeek33@users.noreply.github.com>
2024-12-15 15:18:41 +0100

    Update create-snack.js

--- create-snack.js
+++ create-snack.js
@@ -2,12 +2,14 @@ const { Snack } = require("snack-sdk");
 const fs = require("fs");
 
 // Chargez le code de votre projet
-const code = fs.readFileSync("./App.tsx", "utf8");
+const appTsxCode = fs.readFileSync("./App.tsx", "utf8");
+const appJsCode = `import App from './App.tsx'; export default App;`;
 
 // Créez un nouveau Snack
 const snack = new Snack({
   code: {
-    "App.tsx": code,
+    "App.js": appJsCode,  // Crée un fichier App.js qui importe App.tsx
+    "App.tsx": appTsxCode,  // Utilise ton fichier App.tsx
   },
   dependencies: {
     "expo": "latest",
